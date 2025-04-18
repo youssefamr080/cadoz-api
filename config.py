d2750363f54d859d20d4d47db718c54afd2286c4
El-hoda center <158860190+youssefamr080@users.noreply.github.com>
2025-04-18 18:22:53 +0200

    Add files via upload

--- config.py
+++ config.py
@@ -1,4 +1,6 @@
 # إعدادات الاتصال بقاعدة بيانات MongoDB
-MONGO_URI = 'mongodb+srv://youssefamr080:rvAuEyuvvF3CJGH8@cluster0.qc33p.mongodb.net/?retryWrites=true&w=majority'
-DB_NAME = 'cadoz'
-COLLECTION_NAME = 'products'
+import os
+
+MONGO_URI = os.environ.get('MONGO_URI', 'mongodb://localhost:27017')
+DB_NAME = os.environ.get('DB_NAME', 'cadoz')
+COLLECTION_NAME = os.environ.get('COLLECTION_NAME', 'products')
